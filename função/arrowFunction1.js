c3d248bacea2d8151fe5f0cf2e52a8edf9d1929c
jaimerodriguess <jaimerodriguess@outlook.com>
2020-01-07 13:42:14 -0300

    Arrow1

---
+++ função/arrowFunction1.js
@@ -0,0 +1,19 @@
+/*let dobro = function (a){
+    return 2 * a
+}
+
+dobro = (a) => {
+    return 2 * a
+}
+
+dobro = a => 2 * a
+
+console.log(dobro(4))
+*/
+
+let ola = function(){
+    return 'Olá!'
+}
+
+ola = () => 'Olá'
+console.log(ola())
